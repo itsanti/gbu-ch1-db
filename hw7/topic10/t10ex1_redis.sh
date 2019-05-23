4b93d36d92722ba3ae04ac5116633b3d40804ff5
itsanti <kurov.aleksandr+github@gmail.com>
2019-05-23 15:51:49 +0300

    HW7. topic10 done

--- hw7/topic10/t10ex1_redis.sh
+++ hw7/topic10/t10ex1_redis.sh
@@ -1,12 +1,9 @@
 ######################
-### solution
+### setup
 ######################
 apt-get install redis-server
 systemctl enable redis-server.service
 
-######################
-### result
-######################
 gbu@vbox:~$ sudo systemctl status redis-server.service
 ● redis-server.service - Advanced key-value store
    Loaded: loaded (/lib/systemd/system/redis-server.service; enabled; vendor preset: enabled)
@@ -18,3 +15,22 @@ gbu@vbox:~$ redis-cli
 PONG
 127.0.0.1:6379> quit
 gbu@vbox:~$
+
+######################
+### solution
+######################
+# add IPs
+> HSET ips "127.0.0.1" 0
+> HSET ips "127.0.0.2" 1
+
+# increment value
+> HINCRBY ips "127.0.0.1" 1
+
+######################
+### result
+######################
+> HGETALL ips
+1) "127.0.0.1"
+2) "1"
+3) "127.0.0.2"
+4) "1"
---
+++ hw7/topic10/t10ex2_redis.sh
@@ -0,0 +1,22 @@
+######################
+### solution
+######################
+# add email-name pairs
+> HSET email_name "1@g.com" "alex"
+> HSET email_name "2@g.com" "jhone"
+> HSET email_name "3@g.com" "alex"
+
+> SADD "alex" "1@g.com" "3@g.com"
+
+######################
+### result
+######################
+
+# email-name search
+> HGET email_name "1@g.com"
+"alex"
+
+# name-email search
+> SMEMBERS alex
+1) "3@g.com"
+2) "1@g.com"
